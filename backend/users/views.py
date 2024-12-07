7a38a1822ce25cc2604bbd3d37410e21ccd545a3
Cicero <cicerompase@gmail.com>
2024-12-07 16:53:06 -0300

    adicionando endpoint update/

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,6 @@
 from users.mixins import ExtractUserIdMixin
 from users.models import User
-from users.serializers import UserValidationSerializer, UserInfoSerializer
+from users.serializers import UserValidationSerializer, UserInfoSerializer, UserUpdateSerializer
 
 from django.db import IntegrityError
 from django.shortcuts import get_object_or_404
@@ -36,3 +36,31 @@ class UserInfoView(APIView, ExtractUserIdMixin):
         user = get_object_or_404(User, id=self.user_id)
         serializer = self.serializer_class(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UserUpdateView(APIView, ExtractUserIdMixin):
+    serializer_class = UserUpdateSerializer
+
+    def get(self, request):
+        """
+        Retorna as informações do usuário.
+        """
+        user = get_object_or_404(User, id=self.user_id)
+        data = {
+            "name": user.get_full_name(),  # Assuming 'name' refers to the full name
+            "password": user.password  # Returning the password (NOT recommended in production)
+        }
+        return Response(data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        """
+        Atualiza as informações do usuário.
+        """
+        user = get_object_or_404(User, id=self.user_id)
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            updated_user = serializer.update_user(user, serializer.validated_data)
+            return Response(
+                {"message": "User updated successfully", "user_id": updated_user.id},
+                status=status.HTTP_200_OK
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
